4f94d47942810789f6ba13c2931ea459e62b2bf3
Aleksey Chembrovich <chembrovich0303@mail.ru>
2023-08-08 17:41:07 +0300

    Added blob funcs

--- WebApiTest/Program.cs
+++ WebApiTest/Program.cs
@@ -1,13 +1,23 @@
+using Azure.Storage.Blobs;
+using WebApiTest.Services;
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton(_ =>
+    new BlobServiceClient(builder.Configuration.GetConnectionString("AzureBlob")));
+builder.Services.AddScoped<IBlobService, BlobService>();
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
 app.UseSwagger();
 app.UseSwaggerUI();
 
-app.MapGet("/time", () => Results.Ok(DateTime.UtcNow));
+app.MapGet("/time/utc", () => Results.Ok(DateTime.UtcNow));
+
+app.MapControllers();
 
 app.Run();
---
+++ WebApiTest/Services/BlobService.cs
@@ -0,0 +1,64 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+
+namespace WebApiTest.Services;
+
+public interface IBlobService
+{
+    Task<(byte[] content, string contentType)> GetAsync(string blobName, CancellationToken cancellationToken);
+
+    Task<IReadOnlyCollection<string>> GetListAsync(CancellationToken cancellationToken);
+
+    Task UploadAsync(string blobName, Stream content, string contentType, CancellationToken cancellationToken);
+
+    Task DeleteAsync(string blobName, CancellationToken cancellationToken);
+}
+
+public class BlobService : IBlobService
+{
+    private readonly BlobContainerClient _containerClient;
+
+    private const string _containerName = "test-container";
+
+    public BlobService(BlobServiceClient blobServiceClient)
+    {
+        _containerClient = blobServiceClient.GetBlobContainerClient(_containerName);
+    }
+
+    public async Task<(byte[] content, string contentType)> GetAsync(string blobName, CancellationToken cancellationToken)
+    {
+        var blobClient = _containerClient.GetBlobClient(blobName);
+        var response = await blobClient.DownloadContentAsync(cancellationToken);
+
+        return (response.Value.Content.ToArray(), response.Value.Details.ContentType);
+    }
+
+    public async Task<IReadOnlyCollection<string>> GetListAsync(CancellationToken cancellationToken)
+    {
+        var blobItems = _containerClient.GetBlobsAsync(cancellationToken: cancellationToken);
+        var blobNames = new List<string>();
+        await Parallel.ForEachAsync<BlobItem>(blobItems, cancellationToken, (blobItem, token) =>
+        {
+            blobNames.Add(blobItem.Name);
+            return ValueTask.CompletedTask;
+        });
+
+        return blobNames;
+    }
+
+    public async Task UploadAsync(string blobName, Stream content, string contentType, CancellationToken cancellationToken)
+    {
+        var blobClient = _containerClient.GetBlobClient(blobName);
+        content.Seek(0, SeekOrigin.Begin);
+        _ = await blobClient.UploadAsync(
+            content,
+            new BlobHttpHeaders { ContentType = contentType },
+            cancellationToken: cancellationToken);
+    }
+
+    public async Task DeleteAsync(string blobName, CancellationToken cancellationToken)
+    {
+        var blobClient = _containerClient.GetBlobClient(blobName);
+        _ = await blobClient.DeleteIfExistsAsync(cancellationToken: cancellationToken);
+    }
+}
