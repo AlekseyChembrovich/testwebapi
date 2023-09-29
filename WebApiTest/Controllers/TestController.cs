bfcacb9bb28baae5fb6570eee51fe86d99345c94
Aleksey Chembrovich <chembrovich0303@mail.ru>
2023-09-29 09:31:59 +0300

    Add test controller

---
+++ WebApiTest/Controllers/TestController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiTest.Controllers;
+
+[ApiController]
+[Route("tests")]
+public class TestController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        var result = Enumerable.Range(1, 5)
+            .Select(x => new
+            {
+                Date = DateTime.Now.AddDays(x),
+                Temperature = Random.Shared.Next(-20, 20)
+            })
+            .ToList();
+
+        return Ok(result);
+    }
+}
--- WebApiTest/Program.cs
+++ WebApiTest/Program.cs
@@ -1,13 +1,7 @@
-using Azure.Storage.Blobs;
-using WebApiTest.Services;
-
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddSingleton(_ =>
-    new BlobServiceClient(builder.Configuration.GetConnectionString("AzureBlob")));
-builder.Services.AddScoped<IBlobService, BlobService>();
 builder.Services.AddControllers();
 
 var app = builder.Build();
